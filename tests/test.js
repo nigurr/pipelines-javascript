e2f2e33703152e429aeebbf4cd1831444ab10db2
Abhishek Kumawat <abhishk@microsoft.com>
2019-01-08 11:00:16 +0530

    Adding console output

--- tests/test.js
+++ tests/test.js
@@ -8,5 +8,11 @@ describe('String', function () {
             assert.equal(-1, myString.indexOf('x'));
             assert.equal(-1, myString.indexOf('y'));
         });
+		it('should not return -1 when the substring is present', function () {
+            var myString = 'testxtest';
+
+            assert.notEqual(-1, myString.indexOf('x'));
+            assert.notEqual(-1, myString.indexOf('y'));
+        });
     });
 });
